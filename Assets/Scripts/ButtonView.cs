00bbd276974a8fdea149aa0079e1f14dde7befe7
kazak25 <kazak77709@gmail.com>
2023-03-21 18:03:54 +0300

    Test By Vlad

---
+++ Assets/Scripts/ButtonView.cs
@@ -0,0 +1,20 @@
+using System.Collections;
+using System.Collections.Generic;
+using TMPro;
+using UnityEngine;
+
+public class ButtonView : MonoBehaviour
+{
+    // Start is called before the first frame update
+    [SerializeField] private TextMeshProUGUI _text;
+    void Start()
+    {
+        
+    }
+
+    // Update is called once per frame
+    void Update()
+    {
+        
+    }
+}
