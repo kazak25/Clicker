e0a99aab0cf11d6cc2bea242736327f7bda9624d
MakeZzzzz <changeyourselfyesterday@gmail.com>
2023-03-21 10:45:44 +0300

    test

---
+++ Assets/Scripts/GameManager.cs
@@ -0,0 +1,10 @@
+using System.Collections;
+using System.Collections.Generic;
+using TMPro;
+using UnityEngine;
+
+public class GameManager : MonoBehaviour
+{
+    [SerializeField] private GameObject _gameObject;
+    [SerializeField] private TextMeshProUGUI _text;
+}
